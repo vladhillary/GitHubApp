0755feac0fc73617f4b8687574731ddbbba98470
Vlad <vladhillary@gmail.com>
2021-05-15 19:30:23 +0300

    added header components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import './Components/css/App.css';
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <div className="App">
-      
+      <header className='header'>
+      <Header />
+      </header>
+      <main className='main'>
+
+      </main>
+      <footer className='footer'>
+
+      </footer>
     </div>
   );
 }
